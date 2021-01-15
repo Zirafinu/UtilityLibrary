9e4010eb0122ce7e1b395c53e96ddae5e2bc2057
Zirafinu <focke.sebastian@gmail.com>
2021-01-15 22:53:05 +0100

    added to_array function from std lib c++ 2020

---
+++ parts/general/std_to_array_fwd.hpp
@@ -0,0 +1,29 @@
+/*
+ * std_to_array_fwd.hpp
+ *
+ *  Created on: 15.01.2021
+ *      Author: sebastian
+ */
+
+#ifndef STD_TO_ARRAY_FWD_HPP
+#define STD_TO_ARRAY_FWD_HPP
+
+#include <array>
+
+namespace utility::general::std_fwd {
+
+namespace detail {
+template <class T, std::size_t N, std::size_t... I>
+constexpr std::array<std::remove_cv_t<T>, N> ToArrayImpl(T (&a)[N],//NOLINT
+														 std::index_sequence<I...> /*unused*/) {//NOLINT
+	return { {a[I]...} };//NOLINT
+}
+}
+
+template <class T, std::size_t N>
+constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&a)[N]) {//NOLINT
+	return detail::ToArrayImpl(a, std::make_index_sequence<N>{});//NOLINT
+}
+}  // namespace utility::general::std_fwd
+
+#endif /* STD_TO_ARRAY_FWD_HPP */
